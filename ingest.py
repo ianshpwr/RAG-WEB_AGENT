2c4c2d735e18150e0c954b1398e9a1e32b3d2096
ansh <ianshpwr@gmail.com>
2026-02-08 13:20:41 +0530

    initalised the file strucutre

---
+++ ingest.py
@@ -0,0 +1,16 @@
+from langchain_community.document_loaders import TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+
+loader = TextLoader("data/knowledge.txt")
+docs = loader.load()
+
+splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+chunks = splitter.split_documents(docs)
+
+embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+db = FAISS.from_documents(chunks, embeddings)
+
+db.save_local("db")
+print("✅ Knowledge base created")
